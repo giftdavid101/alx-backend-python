9d6b1fabbe59c06dc8ff5420a4166f94b7fe6d39
giftdavid101 <giftdavid101@gmail.com>
2024-10-15 15:10:49 +0100

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,6 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension()-> typing.List[float]:
+async def async_comprehension() -> typing.List[float]:
     """creates list of 10 numbers"""
-
     return [rand async for rand in  async_generator()]
